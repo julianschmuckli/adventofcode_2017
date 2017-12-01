9367603bd4f3ee3c528b5840731b3f64555c2100
Julian Schmuckli <julian@schmuckli.net>
2017-12-01 10:32:29 +0100

    Added Task 1 of Day 1

---
+++ Day1/src/day1/Day1.java
@@ -0,0 +1,45 @@
+package day1;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Day1 {
+
+    private static File uri;
+    private static String input;
+
+    private static int total_task1 = 0, total_task2 = 0;
+
+    public static void main(String[] args) {
+        uri = new File(args[0]);
+
+        try {
+            input = Files.readAllLines(uri.toPath()).get(0);
+        } catch (IOException ex) {
+            System.out.println("Error reading file");
+        }
+
+        System.out.println(runTask1());
+    }
+
+    public static int runTask1() {
+        String[] arr_input = input.split("");
+        int i = 0;
+        for (String curr : arr_input) {
+            int current = Integer.parseInt(curr);
+            if (i != arr_input.length - 1) {
+                if (current == Integer.parseInt(arr_input[i + 1])) {
+                    total_task1 += current;
+                }
+            }
+            i++;
+        }
+        if (Integer.parseInt(arr_input[arr_input.length - 1]) == Integer.parseInt(arr_input[0])) {
+            total_task1 += Integer.parseInt(arr_input[arr_input.length - 1]);
+        }
+
+        return total_task1;
+    }
+
+}
