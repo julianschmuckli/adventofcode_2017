9f6e5ac1f772ef92600dfd741a2a399997ce93fb
Julian Schmuckli <julian.schmuckli@jeddsan.ch>
2017-12-03 22:46:07 +0100

    Updated file structure

---
+++ Day 1/src/day1/Day1.java
@@ -0,0 +1,67 @@
+package day1;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Day1 {
+
+    private static File uri;
+    private static String input;
+
+    private static int total_task1 = 0, total_task2 = 0;
+
+    public static void main(String[] args) {
+        try{
+            uri = new File(args[0]);
+        }catch(ArrayIndexOutOfBoundsException ex){
+            uri = new File("input.txt"); //Default
+        }
+
+        try {
+            input = Files.readAllLines(uri.toPath()).get(0);
+        } catch (IOException ex) {
+            System.out.println("Error reading file");
+        }
+
+        System.out.println("Solution Task 1: " + runTask1());
+        System.out.println("Solution Task 2: " + runTask2());
+    }
+
+    public static int runTask1() {
+        String[] arr_input = input.split("");
+        int i = 0;
+        for (String curr : arr_input) {
+            int current = Integer.parseInt(curr);
+            if (i != arr_input.length - 1) {
+                if (current == Integer.parseInt(arr_input[i + 1])) {
+                    total_task1 += current;
+                }
+            }
+            i++;
+        }
+        if (Integer.parseInt(arr_input[arr_input.length - 1]) == Integer.parseInt(arr_input[0])) {
+            total_task1 += Integer.parseInt(arr_input[arr_input.length - 1]);
+        }
+
+        return total_task1;
+    }
+
+    public static int runTask2() {
+        String[] arr_input = input.split("");
+        int i = 0;
+        for (String curr : arr_input) {
+            int current = Integer.parseInt(curr);
+            if (i != arr_input.length - 1) {
+                int index = (i + (arr_input.length / 2)) % arr_input.length;
+                if (current == Integer.parseInt(arr_input[index])) {
+                    total_task2 += current;
+                }
+            }
+            i++;
+        }
+
+        return total_task2;
+    }
+
+}
