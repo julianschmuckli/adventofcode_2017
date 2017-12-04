7a00764e107acc90242dd7e9c7a2aeb57e9848f1
Julian Schmuckli <julian@schmuckli.net>
2017-12-04 16:40:52 +0100

    Added Task 2 for Day 4

--- Day 4/src/day4/Day4.java
+++ Day 4/src/day4/Day4.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Day4 {
@@ -47,7 +48,40 @@ public class Day4 {
     }
 
     private static int runTask2() {
-        return 0;
-    }
+        int valid_phrases = 0;
 
+        outerloop:
+        for (String curr_passphrase : input) {
+            ArrayList<ArrayList<Integer>> total_asciisum = new ArrayList<>();
+
+            String[] words = curr_passphrase.split(" ");
+
+            //Add characters to an arraylist and sort it
+            for (String curr_word : words) {
+                char[] chars = curr_word.toCharArray();
+
+                ArrayList<Integer> temp_word = new ArrayList<>();
+                for (char chr : chars) {
+                    temp_word.add((int) chr);
+                }
+                Collections.sort(temp_word);
+                total_asciisum.add(temp_word);
+            }
+
+            int total = 0;
+            for (ArrayList<Integer> curr_outer : total_asciisum) {
+                for (ArrayList<Integer> curr : total_asciisum) {
+                    if (curr.size() == curr_outer.size()) {
+                        if (curr != curr_outer && curr.equals(curr_outer)) {
+                            total++;
+                        }
+                    }
+                }
+            }
+            if (total == 0) {
+                valid_phrases++;
+            }
+        }
+        return valid_phrases;
+    }
 }
