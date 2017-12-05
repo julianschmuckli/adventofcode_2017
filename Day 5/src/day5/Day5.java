af4b0288e3717ee883a35058c63e806391def4d7
Julian Schmuckli <julian.schmuckli@jeddsan.ch>
2017-12-05 21:48:16 +0100

    Added Task 1 for Day 5

---
+++ Day 5/src/day5/Day5.java
@@ -0,0 +1,63 @@
+package day5;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day5 {
+
+    private static File uri;
+    private static List<String> input;
+    private static ArrayList<Integer> jumps = new ArrayList<>();
+
+    public static void main(String[] args) {
+        try {
+            uri = new File(args[0]);
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            uri = new File("input.txt"); //Default
+        }
+
+        try {
+            input = Files.readAllLines(uri.toPath());
+        } catch (IOException ex) {
+            System.out.println("Error reading file");
+        }
+
+        for (String curr : input) {
+            jumps.add(Integer.parseInt(curr));
+        }
+
+        System.out.println("Solution Task 1: " + runTask1());
+        System.out.println("Solution Task 2: " + runTask2());
+    }
+
+    private static int runTask1() {
+        int total_steps = 0;
+        int i = 0;
+        while (true) {
+            int curr = 0;
+            try {
+                curr = jumps.get(i);
+            } catch (IndexOutOfBoundsException ex) {
+                break;
+            }
+
+            if (curr == 0) {
+                jumps.set(i, 1);
+            } else {
+                jumps.set(i, curr + 1);
+                i += curr;
+            }
+            total_steps++;
+        }
+
+        return total_steps;
+    }
+
+    private static int runTask2() {
+        return 0;
+    }
+
+}
