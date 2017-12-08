66d0e82b0315378ffd44f69b2f36d2fee538de1a
Julian Schmuckli <julian@schmuckli.net>
2017-12-08 09:17:37 +0100

    Added Task 2 for Day 8

--- Day 8/src/day8/Day8.java
+++ Day 8/src/day8/Day8.java
@@ -3,6 +3,7 @@ package day8;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,7 +30,7 @@ public class Day8 {
         System.out.println("Solution Task 2: " + runTask2());
     }
 
-    private static String runTask1() {
+    private static int runTask1() {
         HashMap<String, Integer> variables = new HashMap<>();
         for (String curr : input) {
             String[] arr_curr = curr.split(" ");
@@ -107,10 +108,93 @@ public class Day8 {
             }
         }
 
-        return maxEntry.getValue().toString();
+        return maxEntry.getValue();
     }
 
-    private static String runTask2() {
-        return "";
+    private static int runTask2() {
+        HashMap<String, Integer> variables = new HashMap<>();
+        ArrayList<HashMap<String, Integer>> history = new ArrayList<>();
+        for (String curr : input) {
+            String[] arr_curr = curr.split(" ");
+            String variable = arr_curr[0];
+            String how = arr_curr[1];
+            int amount = Integer.parseInt(arr_curr[2]);
+            String if_variable = arr_curr[4];
+            String if_operator = arr_curr[5];
+            int if_amount = Integer.parseInt(arr_curr[6]);
+
+            //Add not existing keys
+            if (!variables.containsKey(variable)) {
+                variables.put(variable, 0);
+            }
+            if (!variables.containsKey(if_variable)) {
+                variables.put(if_variable, 0);
+            }
+
+            if (if_operator.equals("<=")) {
+                if (variables.get(if_variable) <= if_amount) {
+                    if (how.equals("dec")) {
+                        variables.replace(variable, variables.get(variable) - amount);
+                    } else {
+                        variables.replace(variable, variables.get(variable) + amount);
+                    }
+                }
+            } else if (if_operator.equals(">=")) {
+                if (variables.get(if_variable) >= if_amount) {
+                    if (how.equals("dec")) {
+                        variables.replace(variable, variables.get(variable) - amount);
+                    } else {
+                        variables.replace(variable, variables.get(variable) + amount);
+                    }
+                }
+            } else if (if_operator.equals("!=")) {
+                if (variables.get(if_variable) != if_amount) {
+                    if (how.equals("dec")) {
+                        variables.replace(variable, variables.get(variable) - amount);
+                    } else {
+                        variables.replace(variable, variables.get(variable) + amount);
+                    }
+                }
+            } else if (if_operator.equals("==")) {
+                if (variables.get(if_variable) == if_amount) {
+                    if (how.equals("dec")) {
+                        variables.replace(variable, variables.get(variable) - amount);
+                    } else {
+                        variables.replace(variable, variables.get(variable) + amount);
+                    }
+                }
+            } else if (if_operator.equals("<")) {
+                if (variables.get(if_variable) < if_amount) {
+                    if (how.equals("dec")) {
+                        variables.replace(variable, variables.get(variable) - amount);
+                    } else {
+                        variables.replace(variable, variables.get(variable) + amount);
+                    }
+                }
+            } else if (if_operator.equals(">")) {
+                if (variables.get(if_variable) > if_amount) {
+                    if (how.equals("dec")) {
+                        variables.replace(variable, variables.get(variable) - amount);
+                    } else {
+                        variables.replace(variable, variables.get(variable) + amount);
+                    }
+                }
+            }
+
+            HashMap<String, Integer> temp = new HashMap<String, Integer>(variables);
+            history.add(temp);
+        }
+
+        Map.Entry<String, Integer> maxEntry = null;
+
+        for (HashMap<String, Integer> curr : history) {
+            for (Map.Entry<String, Integer> entry : curr.entrySet()) {
+                if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+                    maxEntry = entry;
+                }
+            }
+        }
+
+        return maxEntry.getValue();
     }
 }
